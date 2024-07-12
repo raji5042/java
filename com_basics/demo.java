8e8a73f6e0fe34853ef8f9d24a1828cfcf2350cc
raji5042 <nisarji5042@gmail.com>
2024-07-12 23:16:40 +0530

    Added some java files and made some some changes

---
+++ com_basics/demo.java
@@ -0,0 +1,12 @@
+package com_basics;
+
+import java.sql.SQLOutput;
+
+public class demo {
+    public static void main(String[] args){
+        String channel=" to our";
+        String request=" subscribe";
+        System.out.println("please" + channel + "to our" + request);
+
+    }
+}
